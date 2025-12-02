776370234337f0620f2f60e4f1aea15820852cd7
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-12-02 18:29:55 -0300

    feat: learn const

--- src/hello.go
+++ src/hello.go
@@ -2,24 +2,7 @@ package main
 
 import "fmt"
 
-type WeekDay int
-
-const (
-	Sunday = iota + 1
-	Monday
-	Tuesday
-	Wednesday
-	Thursday
-	Friday
-	Saturday
-)
-
 func main() {
-	fmt.Println(Sunday)
-	fmt.Println(Monday)
-	fmt.Println(Tuesday)
-	fmt.Println(Wednesday)
-	fmt.Println(Thursday)
-	fmt.Println(Friday)
-	fmt.Println(Saturday)
+	const PI float64 = 3.14
+	fmt.Println(PI)
 }
